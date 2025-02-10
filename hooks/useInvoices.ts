87d8f7fed007d8acbe2401bc419d2f388a7686b1
Muhammad Farid Zia <muhammad.paridudin@efishery.com>
2025-02-10 12:20:36 +0800

    feat(add-invoice): add error handling on add and delete invoice

--- hooks/useInvoices.ts
+++ hooks/useInvoices.ts
@@ -28,28 +28,44 @@ export function useInvoices() {
   const addInvoice = (data: InvoiceFormData) => {
     if (state.status !== "success") return;
 
-    const newInvoice: Invoice = {
-      id: Math.random().toString(36).substring(2, 9),
-      createdAt: new Date().toISOString(),
-      ...data,
-      amount: Number.parseFloat(data.amount),
-      invoiceNumber: `INV${data.invoiceNumber}`,
-    };
+    try {
+      const newInvoice: Invoice = {
+        id: Math.random().toString(36).substring(2, 9),
+        createdAt: new Date().toISOString(),
+        ...data,
+        amount: Number.parseFloat(data.amount),
+        invoiceNumber: `INV${data.invoiceNumber}`,
+      };
 
-    const updatedInvoices = [...state.invoices, newInvoice];
-    setState({ status: "success", invoices: updatedInvoices });
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedInvoices));
-    return newInvoice;
+      const updatedInvoices = [...state.invoices, newInvoice];
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedInvoices));
+      setState({ status: "success", invoices: updatedInvoices });
+
+      return newInvoice;
+    } catch {
+      setState({
+        status: "error",
+        error: "Failed to add invoice.",
+      });
+    }
   };
 
   const deleteInvoice = (id: string) => {
     if (state.status !== "success") return;
 
-    const updatedInvoices = state.invoices.filter(
-      (invoice) => invoice.id !== id
-    );
-    setState({ status: "success", invoices: updatedInvoices });
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedInvoices));
+    try {
+      const updatedInvoices = state.invoices.filter(
+        (invoice) => invoice.id !== id
+      );
+
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedInvoices));
+      setState({ status: "success", invoices: updatedInvoices });
+    } catch {
+      setState({
+        status: "error",
+        error: "Failed to delete invoice.",
+      });
+    }
   };
 
   return { state, addInvoice, deleteInvoice };
